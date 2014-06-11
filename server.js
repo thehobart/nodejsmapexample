2894724d39641389969415e0288afed822fe3362
Ryan Malone <ryan@Ryans-iMac.local>
2014-06-11 13:14:35 -0500

    Updated dependancies and modified source to suit

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-var express = require('express'),
-socket = require('socket.io');
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
 
-var app = module.exports = express.createServer();
+//var app = module.exports = express.createServer();
 
 // Configuration
-app.configure(function(){
-  app.use(express.static(__dirname + '/public'));
-});
 
-app.configure('development', function(){
+app.use(express.static(__dirname + '/public'));
+
+
+app.set('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
+app.set('production', function(){
   app.use(express.errorHandler()); 
 });
 
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Express server listening on port %d", app.address().port);
-});
+var port = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 
 //Socket.io
-var io = socket.listen(app);
+//var io = socket.listen(app);
 
 //Needed for Heroku
 //https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
+
+//io.set("transports", ["xhr-polling"]); 
+//io.set("polling duration", 10); 
+
 
 io.on('connection', function(socket){
 
@@ -47,4 +48,7 @@ io.on('connection', function(socket){
 	 
 });
 
+server.listen(port, ipaddr, function() {
+  console.log('%s: Node server started on %s:%d ...', Date(Date.now()), ipaddr, port);
+}); 
 
